02be0aa60bd8c5946cfdd321b09d0950bf61280c
Marghrid <marghrid@hotmail.com>
2019-06-02 20:15:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,12 @@ echo "open docker"
 open -a Docker
 sleep 30
 
+echo "run jupyter on docker"
+osascript -e 'tell app "Terminal"
+   do script "docker run -it -p 8888:8888 -v /Users/ruicruzferreira/Documents/SC:/home/jupyter dockerfile"
+end tell'
+
+sleep 5
+
 echo "open browser"
 open http://localhost:8888/
-
-echo "run jupyter on docker"
-docker run -it -p 8888:8888 -v /Users/ruicruzferreira/Documents/SC:/home/jupyter dockerfile
